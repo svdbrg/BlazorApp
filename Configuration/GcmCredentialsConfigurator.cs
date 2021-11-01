a02c1999a8a542a05ab33538f090bfbdd2f6be1d
Oscar <svdbrg@outlook.com>
2021-11-01 14:50:49 +0100

    Fies

--- Configuration/GcmCredentialsConfigurator.cs
+++ Configuration/GcmCredentialsConfigurator.cs
@@ -2,11 +2,12 @@ public static class GcmCredentialsConfigurator
 {
     public static void ConfigureGoogleCredentials()
     {
+        var dir = Directory.GetCurrentDirectory();
         var filecontent = Environment.GetEnvironmentVariable("gcm.json");
 
-        File.WriteAllText("/gcm.json", filecontent);
+        File.WriteAllText($"{dir}/gcm.json", filecontent);
 
-        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "/gcm.json");
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", $"{dir}/gcm.json");
 
         Console.WriteLine("Credentials: " + Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS"));
     }
