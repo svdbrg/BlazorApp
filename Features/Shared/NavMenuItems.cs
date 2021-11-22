11984a70d5c02f64b4ddc0cc43e484849758a18a
Oscar Svedberg <Oscar.Svedberg@skandia.se>
2021-11-22 15:28:32 +0100

    Adds stuff to supress dotnet warnings

--- Features/Shared/NavMenuItems.cs
+++ Features/Shared/NavMenuItems.cs
@@ -1,11 +1,11 @@
 namespace BlazorApp.Features.Shared;
 
 public class NavMenuItems {
-    public List<NavMenuItem> Items { get; set; }
+    public List<NavMenuItem> Items { get; set; } = new();
 }
 
 public class NavMenuItem {
-    public string Href { get; set; }
-    public string Label { get; set; }
-    public string Icon { get; set; }
+    public string Href { get; set; } = string.Empty;
+    public string Label { get; set; } = string.Empty;
+    public string Icon { get; set; } = string.Empty;
 }
