deaaf24f9ad0c4432327f5a254ae5fd4683159f4
Oscar <svdbrg@outlook.com>
2021-12-16 17:01:52 +0100

    Start moving travel planner to settings modal

--- Features/Shared/Models/AppState.cs
+++ Features/Shared/Models/AppState.cs
@@ -3,7 +3,21 @@ namespace BlazorApp.Features.Shared.Models;
 public class AppState
 {
     public bool AutoRefresh { get; private set; }
+    public string HomeStation { get; private set; } = "300104014";
+    public string DestinationStation { get; private set; } = "300109192";
     public event Action? OnChange;
+
+    public void SetHomeStation(string newStation)
+    {
+        HomeStation = newStation;
+        NotifyStateChanged();
+    }
+
+    public void SetDestination(string newStation)
+    {
+        DestinationStation = newStation;
+        NotifyStateChanged();
+    }
 
     public void ToggleAutoRefresh()
     {
