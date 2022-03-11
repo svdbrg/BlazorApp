7f7fbaec32306b9dc724c0bfd75c7be5cae7c3c4
Oscar <svdbrg@outlook.com>
2022-03-11 11:25:49 +0100

    Adds logging

--- src/Features/Shared/GcpCredentialsConfigurator.cs
+++ src/Features/Shared/GcpCredentialsConfigurator.cs
@@ -5,21 +5,31 @@ public static class GcpCredentialsConfigurator
     public static void AddGoogleCredentials(this WebApplication app, WebApplicationBuilder builder)
     {
         var filePath = "";
-        
+
+        Console.WriteLine("Setting GCP credentials");
+
         if (app.Environment.IsDevelopment())
         {
             filePath = builder.Configuration
                 .GetSection("GoogleCloudPlatform")
                 .GetValue<string>("TokenFilePath");
+
+            Console.WriteLine($"In development, filepath is {filePath}");
         }
         else
         {
+            Console.WriteLine($"In Heroku...");
+
             var dir = Directory.GetCurrentDirectory();
             var filecontent = Environment.GetEnvironmentVariable("gcp.json");
+            Console.WriteLine($"Filecontent: {filecontent}");
 
             File.WriteAllText($"{dir}/gcp.json", filecontent);
 
             filePath = $"{dir}/gcp.json";
+
+            Console.WriteLine($"Filepath is {filePath}");
+
         }
 
         Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filePath);
