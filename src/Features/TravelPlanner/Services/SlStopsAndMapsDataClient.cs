45084013b7e798b0f9fe584a2a0e280e5a80d5d7
Oscar Svedberg <Oscar.Svedberg@skandia.se>
2022-04-05 14:54:08 +0200

    Adds bus lines and stops to memory cache

--- src/Features/TravelPlanner/Services/SlStopsAndMapsDataClient.cs
+++ src/Features/TravelPlanner/Services/SlStopsAndMapsDataClient.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using AutoMapper;
 using BlazorApp.Features.TravelPlanner.Models;
 using BlazorApp.Features.TravelPlanner.Services.Abstractions;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace BlazorApp.Features.TravelPlanner.Services;
 
@@ -9,19 +10,34 @@ public class SlStopsAndMapsDataClient : IStopsAndMapsDataClient
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IMapper _mapper;
+    private readonly IMemoryCache _cache;
 
-    public SlStopsAndMapsDataClient(IHttpClientFactory httpClientFactory, IMapper mapper)
+    public SlStopsAndMapsDataClient(IHttpClientFactory httpClientFactory, IMapper mapper, IMemoryCache cache)
     {
         _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _cache = cache ?? throw new ArgumentNullException(nameof(cache));
     }
 
     public async Task<RouteStopsDto> GetStopsOnLineAsync(string lineNumber)
     {
         using (var client = _httpClientFactory.CreateClient("TravelPlanner"))
         {
-            var linesResponse = await client.GetAsync("https://api.sl.se/api2/LineData.json?key=f18412b0164348ba85445d876debe0d4&model=jour&DefaultTransportModeCode=BUS");
-            var linesResult = JsonSerializer.Deserialize<LinesDTO>(await linesResponse.Content.ReadAsStringAsync());
+            if (!_cache.TryGetValue<LinesDTO>("buslines", out var linesResult))
+            {
+                var linesResponse = await client.GetAsync("https://api.sl.se/api2/LineData.json?key=f18412b0164348ba85445d876debe0d4&model=jour&DefaultTransportModeCode=BUS");
+                linesResult = JsonSerializer.Deserialize<LinesDTO>(await linesResponse.Content.ReadAsStringAsync());
+
+                if (linesResult != null)
+                {
+                    _cache.Set<LinesDTO>(
+                        "buslines",
+                        linesResult,
+                        new MemoryCacheEntryOptions()
+                            .SetSlidingExpiration(TimeSpan.FromDays(1)));
+                }
+            }
+
             var interestingLines = linesResult?.ResponseData.Result.Where(l => l.LineNumber == lineNumber && l.DirectionCode == "1");
 
             if (interestingLines == null)
@@ -31,8 +47,20 @@ public class SlStopsAndMapsDataClient : IStopsAndMapsDataClient
 
             var interestingLineStopNumbers = interestingLines.Select(l => l.JourneyPatternPointNumber).ToList();
 
-            var x = await client.GetAsync("https://api.sl.se/api2/LineData.json?key=f18412b0164348ba85445d876debe0d4&model=stop");
-            var stops = JsonSerializer.Deserialize<StopsDTO>(await x.Content.ReadAsStringAsync());
+            if (!_cache.TryGetValue<StopsDTO>("busstops", out var stops))
+            {
+                var x = await client.GetAsync("https://api.sl.se/api2/LineData.json?key=f18412b0164348ba85445d876debe0d4&model=stop");
+                stops = JsonSerializer.Deserialize<StopsDTO>(await x.Content.ReadAsStringAsync());
+
+                if (stops != null)
+                {
+                    _cache.Set<StopsDTO>(
+                        "busstops",
+                        stops,
+                        new MemoryCacheEntryOptions()
+                            .SetSlidingExpiration(TimeSpan.FromDays(1)));
+                }
+            }
 
             var interestingStops = stops?
                 .ResponseData
